85d6652d94481a15d8f80e4b3e465eb15904d94c
Paul <paul.watson@pdwtech.com>
2015-04-29 07:21:27 +0100

    Use H2 db by default

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 The goal of this Play! sample is to demonstrate a REST API your grandad would be proud of. Features include JPA backed by
 Oracle DB & Swagger integration.
 
-##Prerequisites
+##Switching database to Oracle
 
-* A running Oracle database. 
-  + Oh boy! [Docker](https://www.docker.com) to the rescue, run `docker pull wnameless/oracle-xe-11g`
+* Uncomment the relevant bits on the [application.conf](conf/application.conf)
+* Put your Oracle connection details or use [Docker](https://www.docker.com) to spin up a local instance
+  + `docker pull wnameless/oracle-xe-11g`
   + `docker run -d -p 49160:22 -p 49161:1521 wnameless/oracle-xe-11g`
   + `ssh -p 49160 root@$(boot2docker ip)` (password is admin)
   + `sqlplus system/oracle`
--- app/model/User.java
+++ app/model/User.java
@@ -1,4 +1,4 @@
-package model; 
+package model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,43 +10,50 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Users")
+@Table(name = "Users")
 public class User {
 
-     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, 
-                       generator = "seq")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "seq")
     @SequenceGenerator(name = "seq",
-                      sequenceName = "user_seq")
-     private int id;
-     
- String Name = null;
- String Login = null;
- String Password = null;
+            sequenceName = "user_seq")
+    private int id;
 
-public int getId(){
-     return id;
-}
-public void setId(int id){
-this.id = id;
-}
-public String getName(){
-return Name;
-}
-public void setName(String Name){
-this.Name = Name;
-}
-public String getLogin(){     
-return Login;
-}
-public void setLogin(String Login){
-     this.Login = Login;
-}
-public String getPassword(){
-     return Password;
-}
-public void setPassword(String Password){
-     this.Password = Password;
-}
-     
+    String Name = null;
+    String Login = null;
+    String Password = null;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String Login) {
+        this.Login = Login;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
 }
